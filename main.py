7c02de8e1489657246bc53b7e2ea2c1cef526a64
IDGS-802-23002541 <83322@alumnos.utleon.edu.mx>
2026-01-08 18:02:09 -0600

    d

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def home():
     return "Hola Mundo :DDDDDDDDD"
 
+@app.route('/hola')
+def hola():
+    return "Hola al cuadrado!"
+
 if __name__ == '__main__':
     app.run(debug=True)
